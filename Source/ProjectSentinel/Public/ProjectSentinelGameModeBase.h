ea066736be36fdebabd1c3a9d00db4339d65e21e
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2025-02-25 23:33:15 -0600

    Initial project setup with Git LFS tracking
    Feat: Initial Unreal Engine project setup
    - Added base project structure
    - Configured Git LFS for large Unreal assets
    - Added .gitignore and .gitattributes
    - Committed initial project files

---
+++ Source/ProjectSentinel/Public/ProjectSentinelGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectSentinelGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTSENTINEL_API AProjectSentinelGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
