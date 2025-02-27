1e4478a2e766d24684be05e18b018dca16413ff2
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2025-02-26 19:15:21 -0600

    Created Character class.
    Created Character class in both C++ and Blue print version.
    In Game mode blue print, referenced default pawn class as the newly created character.

---
+++ Source/ProjectSentinel/Private/SentinelRebel.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SentinelRebel.h"
+
+// Sets default values
+ASentinelRebel::ASentinelRebel()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ASentinelRebel::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ASentinelRebel::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ASentinelRebel::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
