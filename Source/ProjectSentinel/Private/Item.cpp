949ac09a62f539b83aa484aecef018763ebeef95
Kedar Chandulur <47148900+KedarChandulur@users.noreply.github.com>
2025-07-25 13:46:23 -0500

    Added (Base) Item class

---
+++ Source/ProjectSentinel/Private/Item.cpp
@@ -0,0 +1,33 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Item.h"
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AItem::AItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	_mItemMesh = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("ItemMesh"));
+	SetRootComponent(_mItemMesh);
+
+	_mCollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
+	_mCollisionBox->SetupAttachment(_mItemMesh);
+}
+
+// Called when the game starts or when spawned
+void AItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
